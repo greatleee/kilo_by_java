04376eeb084efef3bfbb554623dc1ca799e1ca86
greatleee <changdae.lee93@gmail.com>
2020-02-18 14:21:52 +0900

    temporal commit

--- main.java
+++ main.java
@@ -6,43 +6,64 @@ public class main {
     private static String ttyConfig;
 
     public static void main(String[] args) {
+        try {
+            setTerminalToCBreak();
 
-            try {
-                setTerminalToCBreak();
-
-                while (true) {
-                    char[] c = System.console().readPassword();
-                    if (c.length == 1 && c[0] == 'q') {
-                        System.out.println();
-                    }
+            while (true) {
+                int c = 0;
+                c = System.in.read();
+                if (Character.isISOControl(c)) {
+                    System.out.print(c);
+                    System.out.println("\r");
+                } else {
+                    System.out.print(c);
+                    System.out.print(' ');
+                    System.out.print((char) c);
+                    System.out.println("\r");
                 }
+                if (c == 113)
+                    break;
             }
-            catch (IOException e) {
-                    System.err.println("IOException");
-            }
-            catch (InterruptedException e) {
-                    System.err.println("InterruptedException");
-            }
-            finally {
-                try {
-                    stty( ttyConfig.trim() );
-                 }
-                 catch (Exception e) {
-                     System.err.println("Exception restoring tty config");
-                 }
-            }
-
+        }
+        catch (IOException e) {
+                System.err.println("IOException");
+        }
+        catch (InterruptedException e) {
+                System.err.println("InterruptedException");
+        }
+        finally {
+            try {
+                stty( ttyConfig.trim() );
+                }
+                catch (Exception e) {
+                    System.err.println("Exception restoring tty config");
+                }
+        }
     }
 
     private static void setTerminalToCBreak() throws IOException, InterruptedException {
-
         ttyConfig = stty("-g");
-
         // set the console to be character-buffered instead of line-buffered
         stty("-icanon min 1");
-
         // disable character echoing
         stty("-echo");
+        // disalbe ctrl + c and ctrl + z
+        stty("-isig");
+        stty("-iexten");
+
+
+        stty("-brkint");
+        stty("-ixon");
+        stty("-icrnl");
+        stty("-inpck");
+        stty("-istrip");
+
+        stty("-opost");
+
+        stty("cs8");
+
+        stty("min 0");
+        stty("min 1");
     }
 
     /**
